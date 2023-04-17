bb28f61b686a4cec374e78de43395ca803dd4ef9
Anastasia <aastavitskaya@gmail.com>
2023-04-17 11:25:41 +0400

    flask-login

--- blog/app.py
+++ blog/app.py
@@ -3,12 +3,15 @@ from flask import Flask, render_template
 from blog.views.users import users_app
 from blog.views.articles import articles_app
 from blog.models.database import db
+from blog.views.auth import login_manager, auth_app
 
 app = Flask(__name__)
 
 app.register_blueprint(users_app, url_prefix="/users")
 app.register_blueprint(articles_app, url_prefix="/articles")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
+app.register_blueprint(auth_app, url_prefix='/auth')
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////../blog.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
