f198960347e50a6423b59a8cf0fa1683da9abdd1
Anastasia <aastavitskaya@gmail.com>
2023-04-21 10:16:54 +0400

    add db in class User

--- blog/models/user.py
+++ blog/models/user.py
@@ -3,9 +3,12 @@ from blog.models.database import db
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
-    id = Column(Integer, primary_key=True)
-    username = Column(String(80), unique=True, nullable=False)
-    is_staff = Column(Boolean, nullable=False, default=False)
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(250), unique=True)
+    is_staff = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f"<User #{self.id} {self.username!r}>"
