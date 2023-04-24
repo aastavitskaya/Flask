4b45e57495cd8568bcd4d407e876c80fe6bd74a6
Anastasia <aastavitskaya@gmail.com>
2023-04-24 12:55:54 +0400

    registrtion form and register.html

--- blog/forms/user.py
+++ blog/forms/user.py
@@ -1,12 +1,30 @@
-from wtforms import Form, StringField, validators, PasswordField, SubmitField
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators, PasswordField, SubmitField
 
-class UserRegisterForm(Form):
-    first_name =  StringField('First Name')
-    last_name = StringField('Last Name')
-    email = StringField('E-mail' , [validators.DataRequired(), validators.Email()])
-    password = PasswordField('Password', 
-        [validators.DataRequired(),
-        validators.EqualTo('confirm_password')])
-    confirm_password = PasswordField('Password', 
-        [validators.DataRequired()])
-    submit = SubmitField('Register')
+class UserBaseForm(FlaskForm):
+    first_name = StringField("First Name")
+    last_name = StringField("Last Name")
+    username = StringField(
+        "username",
+        [validators.DataRequired()],
+    )
+    email = StringField(
+        "Email Address",
+        [
+            validators.DataRequired(),
+            validators.Email(),
+            validators.Length(min=6, max=200),
+        ],
+        filters=[lambda data: data and data.lower()],
+    )
+
+class RegistrationForm(UserBaseForm):
+    password = PasswordField(
+        "New Password",
+        [
+            validators.DataRequired(),
+            validators.EqualTo("confirm", message="Passwords must match"),
+        ],
+    )
+    confirm = PasswordField("Repeat Password")
+    submit = SubmitField("Register")
