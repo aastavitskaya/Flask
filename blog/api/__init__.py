d6611944fbcbc57ea654519f09b3f6ae8e9b8b77
Anastasia <aastavitskaya@gmail.com>
2023-05-01 14:27:25 +0400

    JSONapi plugin

---
+++ blog/api/__init__.py
@@ -0,0 +1,28 @@
+from flask_combo_jsonapi import Api
+from blog.api.tag import TagList, TagDetail
+from combojsonapi.spec import ApiSpecPlugin
+
+def create_api_spec_plugin(app):
+    api_spec_plugin = ApiSpecPlugin(
+        app=app,
+        # Declaring tags list with their descriptions,
+        # so API gets organized into groups. it's optional.
+        tags={
+            "Tag": "Tag API",
+        }
+    )
+    return api_spec_plugin
+
+def init_api(app):
+    api_spec_plugin = create_api_spec_plugin(app)
+    api = Api(
+        app,
+        plugins=[
+            api_spec_plugin,
+        ],
+    )
+    
+    api.route(TagList, "tag_list", "/api/tags/", tag="Tag")
+    api.route(TagDetail, "tag_detail", "/api/tags/<int:id>/", tag="Tag")
+    
+    return api
--- blog/config.py
+++ blog/config.py
@@ -8,6 +8,9 @@ class BaseConfig(object):
     SECRET_KEY = "lemon_sherbet"
     WTF_CSRF_ENABLED = True
     FLASK_ADMIN_SWATCH = 'sketchy'
+    OPENAPI_URL_PREFIX = '/api/swagger'
+    OPENAPI_SWAGGER_UI_PATH = '/'
+    OPENAPI_SWAGGER_UI_VERSION = '3.22.0'
 
 class DevConfig(BaseConfig):
     DEBUG = True
