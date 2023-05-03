96dc6d06428d1caaebc59825ed9411237b3f64d3
Anastasia <aastavitskaya@gmail.com>
2023-05-03 14:17:47 +0400

    patch_permission for user

--- blog/api/__init__.py
+++ blog/api/__init__.py
@@ -5,7 +5,7 @@ from blog.api.user import UserList, UserDetail
 from blog.api.author import AuthorList, AuthorDetail
 from blog.api.article import ArticleList, ArticleDetail
 from combojsonapi.event import EventPlugin
-
+from combojsonapi.permission import PermissionPlugin
 
 def create_api_spec_plugin(app):
     api_spec_plugin = ApiSpecPlugin(
@@ -24,11 +24,13 @@ def create_api_spec_plugin(app):
 def init_api(app):
     event_plugin = EventPlugin()
     api_spec_plugin = create_api_spec_plugin(app)
+    permission_plugin = PermissionPlugin(strict=False)
     api = Api(
         app,
         plugins=[
             event_plugin,
             api_spec_plugin,
+            permission_plugin,
         ],
     )
     
@@ -38,5 +40,7 @@ def init_api(app):
     api.route(UserDetail, "user_detail", "/api/users/<int:id>/", tag="User")
     api.route(AuthorList, "author_list", "/api/authors/", tag="Author")
     api.route(AuthorDetail, "author_detail", "/api/authors/<int:id>/", tag="Author")
+    api.route(ArticleList, "article_list", "/api/article/", tag="Article")
+    api.route(ArticleDetail, "article_detail", "/api/article/<int:id>/", tag="Article")
     
     return api
